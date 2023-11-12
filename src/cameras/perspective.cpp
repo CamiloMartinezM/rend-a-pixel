b6e778520ea8ec3a816a609bc4a4440e080c7c27
Camilo Martínez M <ca.martinezm2@uniandes.edu.co>
2023-11-12 19:48:09 +0100

    1.1 Perspective Camera Model

--- src/cameras/perspective.cpp
+++ src/cameras/perspective.cpp
@@ -1,4 +1,5 @@
 #include <lightwave.hpp>
+#include <math.h>
 
 namespace lightwave
 {
@@ -13,6 +14,9 @@ namespace lightwave
      */
     class Perspective : public Camera
     {
+    private:
+        double fov, tan_fov, aspect_ratio;
+        
     public:
         Perspective(const Properties &properties)
             : Camera(properties)
@@ -22,15 +26,25 @@ namespace lightwave
             // hints:
             // * precompute any expensive operations here (most importantly trigonometric functions)
             // * use m_resolution to find the aspect ratio of the image
+            aspect_ratio = static_cast<double>(m_resolution.x()) / static_cast<double>(m_resolution.y());
+            fov = (properties.get<float>("fov") / 180.0) * M_PI; // fov converted to radians
+            tan_fov = tan(fov / 2.0);
         }
 
         CameraSample sample(const Point2 &normalized, Sampler &rng) const override
         {
-            // NOT_IMPLEMENTED
-            return CameraSample{.ray = Ray(Vector(normalized.x(), normalized.x(), 0.f), Vector(0.f, 0.f, 1.f)), .weight = Color(1.0f)};
+            // first transforming the normalized coordinates to the local camera coordinate
+            double lc_coord_x = normalized.x() * tan_fov * aspect_ratio;
+            double lc_coord_y = normalized.y() * tan_fov;
+            Vector lc_coord_point =  Vector(lc_coord_x, lc_coord_y, 1.0f);
 
+            // then transforming the rays to world coordinates via the camera transform specified in
+            // the scene description file (stored in m_transform).
             // hints:
             // * use m_transform to transform the local camera coordinate system into the world coordinate system
+            Ray world_coord_ray = m_transform->apply(Ray(Point(0.f, 0.f, 0.f), lc_coord_point.normalized()));
+            
+            return CameraSample{.ray = world_coord_ray, .weight = Color(1.0f)};
         }
 
         std::string toString() const override
